eeabc658aca9368f12608d8af4a896bf6bdcf013
Sriram Sampath <sriramsampath@servion.com>
2020-08-25 11:20:32 +0530

    End of section 9

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,7 +1,6 @@
-import { IPagination } from './models/pagination';
-import { IProduct } from './models/product';
+
 import { Component, OnInit } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+
 
 @Component({
   selector: 'app-root',
@@ -11,16 +10,9 @@ import { HttpClient } from '@angular/common/http';
 export class AppComponent implements OnInit {
 
   title = 'SkiNet';
-  products: IProduct[];
-
-  constructor(private http: HttpClient) { }
+  constructor() { }
 
   ngOnInit(): void {
-    this.http.get('https://localhost:5001/api/products?pageSize=50').subscribe((response: IPagination) => {
-      this.products = response.data;
-      console.log(response);
-    }, error => {
-      console.log(error);
-    });
+
   }
 }
